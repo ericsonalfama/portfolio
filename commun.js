3b88faebba2eb18d13aeb93dfb8dc846780a9d4e
ericsonalfama <102916183+ericsonalfama@users.noreply.github.com>
2022-05-25 23:03:35 +0200

    Add files via upload

---
+++ commun.js
@@ -0,0 +1,11 @@
+const htmlFooter =`
+        
+        <p class= "text-center">
+        ${(new Date()).getFullYear()} - &copy; - Ericson Pereira - <a href="mentions.html">Mention Légales</a>
+        </p>
+` ;
+
+document.querySelector("footer").innerHTML = htmlFooter;
+
+ 
+
